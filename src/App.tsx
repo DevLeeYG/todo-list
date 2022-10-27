4e72531fa32952ce6cc2d3efaccaff72a6755883
hi33741 <hi33741@naver.com>
2022-10-27 21:11:48 +0900

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Styled from 'styled-components';
+const Container = Styled.div`
+  min-height: 100vh;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction:column;
+  
+`;
 
-function App() {
+const Contents = Styled.div`
+  display:flex;
+  background-color: #FFFFFF;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px rgba(0,0,0,0.2);
+`;
+
+const Button = Styled.div`
+  text-align:center;
+  background-color:#304FFE;
+  padding: 10px 20px;
+  border-radius:8px;
+  cursor: pointer;
+  &:hover{
+    background-color: #1E40FF;
+  }
+  &:active{
+    box-shadow: inset 5px 5px 10px rgba(0,0,0,0.2);
+  }
+`;
+
+const Label = Styled.div`
+  color: #FFFFFF;
+  font-size: 16px;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Contents>
+        <Button>
+          <Label>추가</Label>
+        </Button>
+      </Contents>
+    </Container>
   );
-}
+};
 
 export default App;
